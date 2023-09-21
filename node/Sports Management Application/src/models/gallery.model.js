4c270f4d5b0d1fd62951720756b65152662fdf75
freni123 <frenivaghasiya@gmail.com>
2023-09-21 10:34:54 +0530

    21/09/23 gallery model change

--- node/Sports Management Application/src/models/gallery.model.js
+++ node/Sports Management Application/src/models/gallery.model.js
@@ -3,21 +3,13 @@ const mongoose = require("mongoose");
 
 const gallerySchema = new mongoose.Schema(
   {
-    // title: {
-    //   type: String,
-    //   trim: true,
-    // },
-    // description: {
-    //   type: String,
-    //   trim: true,
-    // },
+    title: {
+      type: String,
+      trim: true,
+    },
     image: {
       type: String,
     },
-    // author_name: {
-    //   type: String,
-    //   trim: true,
-    // },
     is_active: {
       type: Boolean,
       default: true,
@@ -26,10 +18,10 @@ const gallerySchema = new mongoose.Schema(
   {
     timestamps: true,
     versionKey: false,
-    toJSON:{
-      transform:function(doc,data){
-        if(data?.image){
-          data.image =`${config.base_url}gallery_images/${data.image}`;
+    toJSON: {
+      transform: function (doc, data) {
+        if (data?.image) {
+          data.image = `${config.base_url}gallery_images/${data.image}`;
         }
       },
     },
